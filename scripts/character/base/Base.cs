f0ac6201bc7925d497f2aa895c0b5f26755a2926
Marc Sanchez <txukyjunior@gmail.com>
2026-03-13 15:52:25 +0100

    Mas iu

---
+++ scripts/character/base/Base.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public partial class Base : Node
+{
+    public static Base Instance;
+
+    public int Health = 100;
+    public int Gold = 200;
+    public int Wood = 50;
+    public int Stone = 0;
+    public int Iron = 0;
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
+
+    public void AddGold(int amount)
+    {
+        Gold += amount;
+    }
+
+    public void DamageBase(int damage)
+    {
+        Health -= damage;
+    }
+
+    public void AddIron(int amount)
+	{
+		Iron += amount;
+	}
+
+	public void AddWood(int amount)
+	{
+		Wood += amount;
+	}
+
+	public void AddStone(int amount)
+	{
+		Stone += amount;
+	}
+}
---
+++ scripts/character/base/Wave.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+public partial class Wave : Node
+{
+    public static Wave Instance;
+
+    public int CurrentWave = 0;
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
+
+    public void StartNextWave()
+    {
+        CurrentWave++;
+    }
+}
